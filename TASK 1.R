614314169320df9e04c27174e31e642af7848f4c
matty1203 <mathews.philip@mermerapps.com>
2021-04-08 22:03:49 +0100

    task1 - date time extracted from dataset1

--- TASK 1.R
+++ TASK 1.R
@@ -1,4 +1,15 @@
+library(dplyr)
 data1<-readRDS('Full_Cassini_Master_MP_BS_CMJ_revised2005.rds', refhook = NULL)
 data2<-readRDS('Cass_data2005.rds', refhook = NULL)
 
 head(data1)
+head(data2)
+#as.POSIXct(paste(data1$))
+max(data1$doy_cross)
+
+##min and max of crossing year
+min(data1$year_cross)
+max(data1$year_cross)
+###converting day into date 
+as.Date(365,origin="2005-01-01")
+
