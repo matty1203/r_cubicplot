87eeba7949efac04f4923cbc99937850d2f236a4
matty1203 <mathews.philip@mermerapps.com>
2021-04-26 02:58:53 +0100

    Dataset Created Commit

--- Task 2 (Data Creation 15 minutes)/Exploratory Analysis on Bow Shock Data.R
+++ Task 2 (Data Creation 15 minutes)/Exploratory Analysis on Bow Shock Data.R
@@ -6,3 +6,8 @@ bowShock<-read.csv('Data/Bow_Shock_Data.csv')
 
 ggplot(bowShock, aes(x=type_cross , y=BTotal.nT.)) + 
   geom_boxplot(notch=TRUE)
+head(sqrt(bowShock$BX_KSM.nT.^2 + bowShock$BY_KSM.nT.^2 + bowShock$BZ_KSM.nT.^2))
+head(bowShock)
+
+ggplot(bowShock, aes(x=X_KSM.km. , y=Y_KSM.km.,color=type_cross)) + 
+  geom_point(pch=19)
